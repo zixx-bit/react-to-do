e722680a6e2129ee794c934eff1a4007f493c687
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-09 13:35:43 +0300

    create an array and store multiple values with  a function that gives a return value

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
- import "./style.css";
+ import { useState } from "react";
+import "./style.css";
  
  export default function App() {
+   const [newItem, setNewItem] = useState("")
+   const [todos, setTodos] = useState([])
+
+   function handleSubmit(e){
+    e.preventDefault()
+
+
+     setTodos(currentTodos =>{
+        return[
+            ...currentTodos,
+            {
+                id:crypto.randomUUID(), title: newItem, completed:false
+
+            },
+        ]
+     } )   
+   
+        console.log(todos)
+   }
+
+   
+   
   return <>
-        <form className="new-item-item"></form>
+        <form onSubmit={handleSubmit} className="new-item-form">
+            <div className="form-row">
+            <label htmlFor="item">New Item</label>
+            <input type="text" value={newItem} onChange={e => setNewItem(e.target.value )} id="item"/>            
+            <button className="btn">Add</button>
+            </div>
+        </form>
+
+        <h1 className="header">Todo List</h1>
+        <input type="text" value={newItem} onChange={e => setNewItem(e.target.value )} id="item"/>            
+
+        <ul className="list">
+            <li>
+                <label>
+                <input type="checkbox"/>
+
+                    Item 1                
+                </label>
+                <button className="btn btn-danger">Delete</button>
+            </li>
+            <li>
+                <label>
+                <input type="checkbox"/>
+                    Item 1                
+                </label>
+                <button className="btn btn-danger">Delete</button>
+            </li>
+            
+        </ul>
   </> 
  }
